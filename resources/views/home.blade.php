94cccea6eccf38fa17747026207e00ff589f6643
Jason Polito <jasonpolito@gmail.com>
2025-07-15 10:05:37 -0400

    deploy

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,20 +6,23 @@
     Laravel and Vite.')
 
 @section('content')
-    <div class="align-center flex h-screen flex-col justify-center">
-        <h1 class="text-center text-9xl font-bold"><x-word-rotator :words="[
-            'UX+UI',
-            'Laravel',
-            'React',
-            'Tailwind',
-            'Animations',
-            'Vue',
-            'Svelte',
-            'PHP',
-            'Print Media',
-            'WordPress',
-            'Databases',
-        ]"></x-word-rotator>
-        </h1>
+    <div class="align-center flex h-screen flex-col justify-center text-center">
+        <div class="space-y-8">
+            <h3 class="text-sm uppercase tracking-widest text-slate-500">The Definition of Full-stack</h3>
+            <h1 class="text-center text-9xl font-bold"><x-word-rotator :words="[
+                'UX+UI',
+                'Laravel',
+                'React',
+                'Tailwind',
+                'Animations',
+                'Vue',
+                'Svelte',
+                'PHP',
+                'Print Media',
+                'WordPress',
+                'Databases',
+            ]"></x-word-rotator>
+            </h1>
+        </div>
     </div>
 @endsection

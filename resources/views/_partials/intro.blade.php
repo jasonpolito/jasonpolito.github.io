cd9245b204e92d86c1d8dae276dfddff9be0516b
Jason Polito <jasonpolito@gmail.com>
2025-07-17 13:03:16 -0400

    deploy

---
+++ resources/views/_partials/intro.blade.php
@@ -0,0 +1,20 @@
+<x-card>
+    <div>
+        <div class="gap-x-8 gap-y-2 uppercase leading-[0.9] text-red-800 sm:flex">
+            <div class="w-full">
+                <h3 class="fitty">Jason</h3>
+            </div>
+            <div class="w-full">
+                <h3 class="fitty">Polito</h3>
+            </div>
+        </div>
+        <h3 class="relative my-3 block w-full rounded-md bg-current px-2 py-[0.25rem] sm:px-4">
+            <div class="relative left-1 sm:left-[1rem]">
+                <div class="fitty tracking-[0.5em] text-white sm:tracking-[1em]">
+                    Full-Stack
+                </div>
+            </div>
+        </h3>
+        <h3 class="fitty -mb-4 -mt-2 text-current">Web Developer</h3>
+    </div>
+</x-card>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,28 +26,8 @@
 
 @section('content')
     <x-container class="font-display text-{{ $ink }} max-w-3xl uppercase">
-        @for ($i = 0; $i < 3; $i++)
-            <x-card>
-
-                <div>
-                    <div class="gap-x-8 gap-y-2 uppercase leading-[0.9] text-red-800 sm:flex">
-                        <div class="w-full">
-                            <h3 class="fitty">Jason</h3>
-                        </div>
-                        <div class="w-full">
-                            <h3 class="fitty">Polito</h3>
-                        </div>
-                    </div>
-                    <h3 class="bg-{{ $ink }} relative my-3 block w-full rounded-md px-2 py-[0.25rem] sm:px-4">
-                        <div class="relative left-1 sm:left-[1rem]">
-                            <div class="fitty tracking-[0.5em] text-white sm:tracking-[1em]">
-                                Full-Stack
-                            </div>
-                        </div>
-                    </h3>
-                    <h3 class="fitty text-{{ $ink }} -mb-4 -mt-2">Web Developer</h3>
-                </div>
-            </x-card>
-        @endfor
+        @include('_partials.intro')
+        <div class="text-blue-950">@include('_partials.intro')</div>
+        <div class="text-emerald-950">@include('_partials.intro')</div>
     </x-container>
 @endsection

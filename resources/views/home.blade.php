a37d02294dbb1be7f7474a329d7526dc6599adce
Jason Polito <jasonpolito@gmail.com>
2025-07-17 10:39:37 -0400

    deploy

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,8 +26,8 @@
 
 @section('content')
     <x-container class="font-display max-w-3xl uppercase">
-        <x-painterly>
-            <x-container class="border-16 border-cyan-950">
+        <x-painterly class="p-4">
+            <x-container class="outline-16 rounded-lg outline-cyan-950">
                 <div>
                     <div class="gap-x-8 gap-y-2 uppercase leading-[0.9] text-cyan-950 sm:flex">
                         <div class="w-full">
@@ -42,10 +42,8 @@
                             <div class="fitty relative -right-[0.45rem] tracking-[0.5em]">Full-Stack</div>
                         </div>
                     </h3>
-                    <h3 class="fitty -my-2 text-cyan-950">Web
-                        Developer</h3>
+                    <h3 class="fitty -my-2 text-cyan-950">Web Developer</h3>
                 </div>
-
             </x-container>
         </x-painterly>
     </x-container>

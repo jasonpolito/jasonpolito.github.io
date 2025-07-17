1fab5941343e895bfa531243c4cb3de95bff5753
Jason Polito <jasonpolito@gmail.com>
2025-07-17 18:57:23 -0400

    deploy

---
+++ resources/views/_partials/_skills.blade.php
@@ -0,0 +1,6 @@
+<x-card class="bg-current text-lime-950">
+    {{-- <x-card class="bg-{{ $ink }}"> --}}
+    @include('_partials._desktop_skills')
+    @include('_partials._tablet_skills')
+    @include('_partials._mobile_skills')
+</x-card>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,13 +14,9 @@
                     Decade of Experience <span class="relative -top-1 text-[0.6em]">✦</span></h4>
             </div>
         </x-card>
-        <div class="relative mx-auto -mt-2 mb-6 w-3/5 sm:-mt-4 sm:mb-8 sm:w-2/5 lg:-mt-10">
+        <div class="relative mx-auto -mt-2 mb-6 w-3/5 text-yellow-950 sm:-mt-4 sm:mb-8 sm:w-2/5 lg:-mt-10">
             @include('_partials.horse1')
         </div>
-        <x-card class="bg-{{ $ink }}">
-            @include('_partials._desktop_skills')
-            @include('_partials._tablet_skills')
-            @include('_partials._mobile_skills')
-        </x-card>
+        @include('_partials._skills')
     </x-container>
 @endsection

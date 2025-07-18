cd9985f8796a3465c13b9e721b52d26159011353
Jason Polito <jasonpolito@gmail.com>
2025-07-18 11:13:08 -0400

    deploy

--- resources/views/_layouts/main.blade.php
+++ resources/views/_layouts/main.blade.php
@@ -19,7 +19,13 @@
     <x-painterly class="relative block min-h-dvh">
         @yield('content')
         <div class="pointer-events-none absolute inset-0 size-full"
-            style="background-image: url({{ asset('assets/images/texture.png') }})">
+            style="background-image: url({{ asset('assets/images/asfalt-light.png') }})">
+        </div>
+        <div class="pointer-events-none absolute inset-0 size-full scale-x-[-1]"
+            style="background-image: url({{ asset('assets/images/asfalt-dark.png') }})">
+        </div>
+        <div class="pointer-events-none absolute inset-0 size-full"
+            style="background-image: url({{ asset('assets/images/canvas.png') }})">
         </div>
     </x-painterly>
     @stack('script')

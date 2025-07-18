5a8fca4770bc39c8415cbddf52bb89b2aaab9da9
Jason Polito <jasonpolito@gmail.com>
2025-07-18 17:41:50 -0400

    deploy

--- resources/views/_partials/_about.blade.php
+++ resources/views/_partials/_about.blade.php
@@ -12,58 +12,51 @@
     </div>
 </x-card> --}}
 
+@php
+    $highlightedWords = [
+        'full&#8209;stack',
+        'Laravel',
+        'Vue',
+        'React',
+        'PHP',
+        'Tailwind',
+        'Node',
+        'databases',
+        'logos',
+        'animated',
+        'animating',
+        'animation',
+        'servers',
+        'Nuxt',
+        'cartwheels',
+    ];
+@endphp
+
 <x-card class="-mt-3 text-transparent sm:-mt-6">
     <div class="flex flex-wrap gap-3 sm:flex-nowrap sm:gap-6">
         <div class="sm:w-1/2">
             <p class="text-{{ $dim }} text-justify normal-case leading-[1.6rem] md:text-xl">
-                @highlightWords([
-                    'full&#8209;stack',
-                    'Laravel',
-                    'Vue',
-                    'React',
-                    'PHP',
-                    'Tailwind',
-                    'Node',
-                    'databases',
-                    'logos',
-                    'animated',
-                    'servers',
-                    'Nuxt',
-                    'cartwheels'
-                ])
-                    Well now, saddle up and let me tell you a tale — I'm a full&#8209;stack wrangler with one boot in design
-                    and
-                    the other in code, ridin' clean from Laravel gulches through the Vue and React ranges. I rope in PHP
-                    like a
-                    seasoned ranch hand, style with the precision of Tailwind, and handle Node and databases like a poker
-                    game I
-                    never lose.
+                @highlightWords($highlightedWords)
+                    Saddle up partner and let me tell you a tale — I'm a full&#8209;stack wrangler with one boot in code
+                    and the other in design. Ridin' clean from Laravel and PHP gulches through the Vue and React ranges. I
+                    rope in
+                    Tailwind like a seasoned ranch hand and handle databases like a poker game I never lose.
                 @endHighlightWords
             </p>
         </div>
         <div class="sm:w-1/2">
             <p class="text-{{ $dim }} text-justify normal-case leading-[1.6rem] md:text-xl">
-                @highlightWords([
-                    'full&#8209;stack',
-                    'Laravel',
-                    'Vue',
-                    'React',
-                    'PHP',
-                    'Tailwind',
-                    'Node',
-                    'databases',
-                    'logos',
-                    'animated',
-                    'servers',
-                    'Nuxt',
-                    'cartwheels'
-                ])
-                    I've branded logos, animated campfire stories, set up servers sturdier than a saloon safe,
-                    and
-                    even dabbled in the fine print arts. Nuxt? Tamed it. Cartwheels? I do 'em just for flair. Whether it's
-                    pixels or pipelines, I'm your all-terrain tech cowboy.
+                @highlightWords($highlightedWords)
+                    From branding logos, to animating camp fires and stirring up servers sturdier than a saloon safe!
+                    Whether it's pixels or pipelines, I'm your full&#8209;stack, all-terrain tech cowboy.
                 @endHighlightWords
             </p>
+            <x-card class="text-{{ $accent }} relative bg-current text-xs">
+                <div class="-mx-6 flex items-center justify-center gap-3 text-white">
+                    <h4 class="text-xl">Say howdy! </h4>
+                    <div class="w-12">@include('_partials.point')</div>
+                </div>
+            </x-card>
         </div>
     </div>
 </x-card>

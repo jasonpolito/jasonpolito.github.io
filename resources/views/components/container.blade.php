c91cd36057197529fe0a9cacfe9e978934ab8115
Jason Polito <jasonpolito@gmail.com>
2025-07-19 14:53:29 -0400

    deploy

--- resources/views/components/container.blade.php
+++ resources/views/components/container.blade.php
@@ -1,3 +1,3 @@
-<div {{ $attributes->merge(['class' => 'container mx-auto p-4 md:p-8 xl:p-12 relative']) }}>
+<div {{ $attributes->merge(['class' => 'container mx-auto p-4 lg:p-12 relative']) }}>
     {{ $slot }}
 </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,43 +6,41 @@
     Laravel and Vite.')
 
 @section('content')
-    <x-container
-        class="font-display text-{{ $ink }} from-{{ $paperDim }} via-{{ $paper }} to-{{ $paperDim }} outline-16 outline-{{ $accent }} relative max-w-3xl rounded bg-gradient-to-r">
-        @include('_partials.intro')
-        <x-card class="text-transparent">
-            <div class="text-{{ $dim }} -mt-6 sm:-mt-8 lg:-mt-12">
-                <h4 class="fitty nowrap-forced tracking-widest"><span class="relative -top-1 text-[0.6em]">✦</span> Over a
-                    Decade of Experience <span class="relative -top-1 text-[0.6em]">✦</span></h4>
-            </div>
-        </x-card>
-        <div class="relative mx-auto -mt-2 mb-6 w-3/5 sm:-mt-4 sm:mb-8 sm:w-2/5 lg:-mt-10">
-            <div class="fill-parent left-16! scale-[0.9] text-cyan-950">@include('_partials.horse1')</div>
-            <div class="fill-parent -left-16! scale-[0.9] text-red-800">@include('_partials.horse1')</div>
-            <div class="relative">@include('_partials.horse1')</div>
-        </div>
-
-        <x-card class="text-transparent">
-            <div class="text-{{ $dim }} -mt-6 sm:-mt-8 lg:-mt-12">
-                <h4 class="fitty nowrap-forced tracking-widest"><span class="relative -top-1 text-[0.6em]">✦</span>
-                    Done Pertin' Near Everything <span class="relative -top-1 text-[0.6em]">✦</span></h4>
-            </div>
-        </x-card>
-
-        <div class="lg:-mt-15 -mt-6 sm:-mt-9">
-            <x-card class="text-{{ $ink }} bg-current">
-                <div class="text-white">@include('_partials._skills')</div>
+    <div class="sm:p-8 lg:p-0">
+        <x-container
+            class="font-display text-{{ $ink }} from-{{ $paperDim }} via-{{ $paper }} to-{{ $paperDim }} outline-16 outline-{{ $accent }} relative max-w-3xl rounded bg-gradient-to-r">
+            @include('_partials.intro')
+            <x-card class="text-transparent">
+                <div class="text-{{ $dim }} -mt-6 sm:-mt-8 lg:-mt-12">
+                    <h4 class="fitty nowrap-forced tracking-widest"><span class="relative -top-1 text-[0.6em]">✦</span> Over a
+                        Decade of Experience <span class="relative -top-1 text-[0.6em]">✦</span></h4>
+                </div>
             </x-card>
-        </div>
-
-        @include('_partials._about')
-
-        <div class="text-{{ $dim }} pt-8 text-center text-xs">&copy; {{ date('Y') }} – Jason Polito</div>
-        @foreach (['top', 'bottom'] as $item)
-            @for ($i = 0; $i < 2; $i++)
-                <div
-                    class="text-{{ $dim }} {{ $item }}-0 {{ $i ? 'left' : 'right' }}-0 leading-2 absolute p-2">
-                    ✦</div>
-            @endfor
-        @endforeach
-    </x-container>
+            <div class="relative mx-auto -mt-2 mb-6 w-3/5 sm:-mt-4 sm:mb-8 sm:w-2/5 lg:-mt-10">
+                <div class="fill-parent left-16! scale-[0.9] text-cyan-950">@include('_partials.horse1')</div>
+                <div class="fill-parent -left-16! scale-[0.9] text-red-800">@include('_partials.horse1')</div>
+                <div class="relative">@include('_partials.horse1')</div>
+            </div>
+            <x-card class="text-transparent">
+                <div class="text-{{ $dim }} -mt-6 sm:-mt-8 lg:-mt-12">
+                    <h4 class="fitty nowrap-forced tracking-widest"><span class="relative -top-1 text-[0.6em]">✦</span>
+                        Done Pertin' Near Everything <span class="relative -top-1 text-[0.6em]">✦</span></h4>
+                </div>
+            </x-card>
+            <div class="lg:-mt-15 -mt-6 sm:-mt-9">
+                <x-card class="text-{{ $ink }} bg-current">
+                    <div class="text-white">@include('_partials._skills')</div>
+                </x-card>
+            </div>
+            @include('_partials._about')
+            <div class="text-{{ $dim }} pt-8 text-center text-xs">&copy; {{ date('Y') }} – Jason Polito</div>
+            @foreach (['top', 'bottom'] as $item)
+                @for ($i = 0; $i < 2; $i++)
+                    <div
+                        class="text-{{ $dim }} {{ $item }}-0 {{ $i ? 'left' : 'right' }}-0 leading-2 absolute p-2">
+                        ✦</div>
+                @endfor
+            @endforeach
+        </x-container>
+    </div>
 @endsection

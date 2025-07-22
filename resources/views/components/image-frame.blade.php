cf44e96cbda5f54f15f7fd230e981ffc3e36623a
Jason Polito <jasonpolito@gmail.com>
2025-07-21 23:00:39 -0400

    deploy

--- resources/views/components/image-frame.blade.php
+++ resources/views/components/image-frame.blade.php
@@ -2,17 +2,20 @@
     $id = uniqid('image-frame-');
 @endphp
 <div
-    class="relative block rounded shadow-sm outline-none appearance-none cursor-pointer padding-0 hover:z-1 group shadow-amber-950/10 hover:shadow-lg hover:shadow-amber-950/20 focus:z-10 focus:shadow-lg focus:shadow-amber-950/20">
-    <div class="bg-{{ $ink }} flex gap-0.5 rounded-t p-1 group-hover:bg-slate-700">
+    class="relative block rounded shadow-sm outline-none appearance-none padding-0 group cursor-zoom-in shadow-amber-950/10 hover:shadow-lg hover:shadow-amber-950/20 focus:z-10 focus:shadow-lg focus:shadow-amber-950/20">
+    <div class="bg-{{ $ink }} flex gap-0.5 rounded-t p-1 group-hover:bg-slate-950">
         @foreach (['red', 'amber', 'lime'] as $color)
             <div
                 class="group-hover:bg-{{ $color }}-400 group-focus:bg-{{ $color }}-400 bg-{{ $paper }} size-1 rounded-full">
             </div>
         @endforeach
     </div>
-    <div
-        class="border-3 border-{{ $ink }} bg-{{ $paper }} rounded-b group-hover:border-slate-700 group-focus:border-slate-700">
-        <img src="{{ asset($src) }}" alt=""
-            class="filter-[url(#posterizer)_saturate(0)_sepia(0.06)_invert(100)] relative mix-blend-multiply group-hover:mix-blend-normal group-hover:contrast-100 group-hover:saturate-100 group-hover:sepia-0 group-focus:mix-blend-normal group-focus:contrast-100 group-focus:saturate-100 group-focus:sepia-0">
+    <div class="border-3 border-{{ $ink }} bg-{{ $paper }} rounded-b">
+        <div class="relative">
+            <img src="{{ asset($src) }}" alt=""
+                class="filter-[url(#posterizer)_saturate(0)_sepia(0.06)_invert(100)] mix-blend-multiply">
+            <img src="{{ asset($src) }}" alt=""
+                class="relative transition duration-700 opacity-0 pointer-events-none z-1 fill-parent group-hover:opacity-100">
+        </div>
     </div>
 </div>

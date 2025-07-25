dd006a11353842f8a3bd106a5a61293c8cf7414a
Jason Polito <jasonpolito@gmail.com>
2025-07-25 14:43:28 -0400

    deploy

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,8 @@
 // import "./particleBackground";
 import fitty from "fitty";
-import "./wordRotator";
+// import "./wordRotator";
 // import "./_tailwindColors";
-import Macy from "macy";
+// import Macy from "macy";
 
 const fitties = fitty(".fitty", {
     minSize: 0,
@@ -11,24 +11,24 @@ const fitties = fitty(".fitty", {
 
 import "./painterly";
 
-setTimeout(() => {
+setInterval(() => {
     fitties.forEach((fittyInstance) => {
         fittyInstance.fit();
     });
-}, 10);
+}, 500);
 
-document.querySelectorAll(".macy").forEach((el) => {
-    const macyInstance = Macy({
-        container: el,
-        trueOrder: false,
-        waitForImages: true,
-        margin: 12,
-        columns: 3,
-        breakAt: {
-            1024: 3,
-            768: 2,
-            640: 1,
-        },
-    });
-    console.log(macyInstance);
-});
+// document.querySelectorAll(".macy").forEach((el) => {
+//     const macyInstance = Macy({
+//         container: el,
+//         trueOrder: false,
+//         waitForImages: true,
+//         margin: 12,
+//         columns: 3,
+//         breakAt: {
+//             1024: 3,
+//             768: 2,
+//             640: 1,
+//         },
+//     });
+//     console.log(macyInstance);
+// });

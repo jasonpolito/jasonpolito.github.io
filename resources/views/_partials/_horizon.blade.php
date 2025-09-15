99d659e4bc77fc53465b3bef9156b11949858688
Jason Polito <jasonpolito@gmail.com>
2025-09-15 09:07:03 -0400

    desert bluffs and clouds adjustments

--- resources/views/_partials/_horizon.blade.php
+++ resources/views/_partials/_horizon.blade.php
@@ -1,9 +1,9 @@
 <div class="absolute bottom-0 left-0 h-1/2 w-full text-slate-950">
     <div class="relative mx-auto h-full max-w-[1480px]">
-        <div class="absolute bottom-[-20px] left-[-5%] w-[600px] text-red-800">@include('_partials.svg.bluffs._bluff2')</div>
-        <div class="absolute bottom-[-20px] right-0 w-[600px] text-red-800">@include('_partials.svg.bluffs._bluff1')</div>
-        <div class="absolute bottom-[50%] left-[-10%] w-[600px]">@include('_partials.svg.clouds._cloud1')</div>
-        <div class="absolute bottom-[80%] right-[-5%] w-[350px]">@include('_partials.svg.clouds._cloud2')</div>
-        <div class="absolute bottom-[100%] right-[5%] w-[350px]">@include('_partials.svg.clouds._cloud3')</div>
+        <div class="absolute bottom-[-20px] left-[-5%] w-[600px] text-red-700">@include('_partials.svg.bluffs._bluff2')</div>
+        <div class="absolute bottom-[-20px] right-0 w-[600px] text-red-700">@include('_partials.svg.bluffs._bluff1')</div>
+        <div class="absolute bottom-[50%] right-[-10%] w-[600px] text-red-700">@include('_partials.svg.clouds._cloud1')</div>
+        <div class="absolute bottom-[105%] left-[-5%] w-[350px] text-slate-950">@include('_partials.svg.clouds._cloud2')</div>
+        <div class="absolute bottom-[120%] left-[5%] w-[350px] text-slate-950">@include('_partials.svg.clouds._cloud3')</div>
     </div>
 </div>
